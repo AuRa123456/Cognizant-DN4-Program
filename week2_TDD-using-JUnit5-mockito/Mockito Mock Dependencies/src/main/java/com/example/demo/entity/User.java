36a17481d877b5b788a6389bf1c7ed1d90618c06
AuRa123456 <prabyasachi2004mohanty@gmail.com>
2025-06-29 19:42:00 +0530

    uploaded mockito mock exercises

---
+++ week2_TDD-using-JUnit5-mockito/Mockito Mock Dependencies/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,18 @@
+
+package com.example.demo.entity;
+
+import jakarta.persistence.Id;
+
+public class User {
+
+    @Id
+    private Long id;
+    private String name;
+
+    // getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
